5579bd7af210e662db65a79f7c54ee781b3b2e3a
Blue-Latios <13516066@std.stei.itb.ac.id>
2021-12-02 01:52:32 +0700

    Initial version

---
+++ pages/index.vue
@@ -0,0 +1,82 @@
+<template><div class="container">
+  <div class="text-bold">Text format:<br>
+  </div>
+  
+  Use <u>CATEGORY:</u> followed by name to make a category.<br>
+  <br>
+  Example wishlist:<br>
+  CATEGORY: Genes<br>
+  Primary Gene: Jaguar<br>
+  Secondary Gene: Blend<br>
+  Tertiary Gene: Capsule<br>
+  CATEGORY: Misc.<br>
+  Greattusk<br>
+  Rotala<br><br>
+  
+  <div class="text-bold">Enter your items text here.<br><br>
+  </div>
+  <textarea v-model="textStuff" placeholder="Write your items in Notepad where you can save and edit for later, and copy paste it here." style="width:90%;"></textarea><br>
+  Column number: <select v-model="columnNo">
+  <option>3</option>
+  <option>4</option>
+  <option>5</option>
+  <option>6</option>
+  <option>7</option>
+  </select>
+  <button @click="generate">Generate BBCode</button><br><br>
+  Copy everything from here:<br>
+  <p class="preformat">{{ result }}</p>
+</div></template>
+
+<script>
+export default {
+	data() {
+		return {
+			result: '',
+			textStuff: '',
+			columnNo: 4,
+		};
+	},
+	methods: {
+		generate() {
+			var s = '[center]';
+			var sArray = this.textStuff.split('\n');
+			var col = 0;
+			sArray.forEach(x => {
+				if (x.substring(0,9) === "CATEGORY:") {
+					s = s.concat("\n\n[size=5][b]", x.substring(10,x.length), "[/b][/size]\n-----\n");
+					col = 0;
+				}
+				else {
+					s = s.concat("[item=", x, "]");
+					col++;
+					if (col == this.columnNo) {
+						col = 0;
+						s = s.concat("\n");
+					}
+				}
+			});
+			this.result=s.concat("[/center]");
+		},
+	}
+}
+</script>
+
+var 
+
+<style>
+.container {
+	padding: 10px;
+	margin: 5px;
+	font-size: 14px;
+}
+.text-bold {
+	font-weight: bold;
+	font-size: 20px;
+}
+.preformat {
+	white-space: pre-wrap;
+	padding: 5px;
+	font-size: 12px;
+}
+</style>
